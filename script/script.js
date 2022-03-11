de945d0917cf0ec3f762bc1d85918fb11bee614d
itcube-vn <85250669+itcube-vn@users.noreply.github.com>
2022-03-11 13:12:19 +0300

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	$('.slider').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 10000,
+		fade: true,
+		arrows: false,
+		dots: true
+	});
+});
